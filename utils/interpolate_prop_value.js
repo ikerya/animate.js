f9ca4075fabed5d15ff8630bba9179129b8d55ed
ikerya <49207045+ikerya@users.noreply.github.com>
2023-07-24 21:01:14 +0300

    fixed bug with scale3d property

--- utils/interpolate_prop_value.js
+++ utils/interpolate_prop_value.js
@@ -2,6 +2,7 @@ export default function interpolatePropValue(prop, value) {
     switch (prop) {
         case 'opacity':
         case 'scale':
+        case 'scale3d':
             return value.toFixed(3);
         default:
             return Math.floor(value);
